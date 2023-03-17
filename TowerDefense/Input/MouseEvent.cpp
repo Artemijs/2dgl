c2d0a9b08d46404768d9da9bf3b47623cfb5dfb6
unknown <artemijspoznaks@gmail.com>
2023-03-17 19:32:36 +0000

    Removed Tower Defense project, deciding to work within the 2dgl project itself, in a PROJECTS folder instead

--- TowerDefense/Input/MouseEvent.cpp
+++
@@ -1,8 +0,0 @@
-#include "MouseEvent.h"
-#include "MouseEventController.h"
-#include "../Util/Callback.h"
-MouseEvent::MouseEvent(Bounds* bounds, BaseObject * bo) {
-	_bounds = bounds;
-	bo->AddCallback(new BBCall(_bounds, bo->GetModelMatrix()));
-	MouseEventController::RegisterEvent(this);
-}
--- TowerDefense/Util/Callback.cpp
+++
@@ -1,9 +0,0 @@
-#include "Callback.h"
-
-BBCall::BBCall(Bounds* b, Matrix4x4* m) {
-	_bounds = b;
-	_model = m;
-}
-void BBCall::Execute() {
-	_bounds->Translate2World((*_model));
-}
